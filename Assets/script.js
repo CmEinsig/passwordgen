7ef753a20c77b2fa26b743dfdfd46d3928d54a19
CmEinsig <cmneiderer@gmail.com>
2023-03-23 23:42:13 -0400

    got window prompts to display

--- Assets/script.js
+++ Assets/script.js
@@ -11,18 +11,22 @@ lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
 "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+numbers = [1,2,3,4,5,6,7,8,9]
 special = [" !@#$%^&*()"]
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var generatepassword =function () {
+var length = window.prompt("Enter a password length between 8 and 128 characters")
+var lowercase = window.prompt ("Do you want lowercase letters?")
+var uppercase = window.prompt ("Do you want uppercase letters?")
+var numbers = window.prompt ("Do you want to use numbers?")
+var special = window.prompt ("Do you want to use special characters?")
 
-  passwordText.value = password;
+ //passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
 
 generatepassword ()
