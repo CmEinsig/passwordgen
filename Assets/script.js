e5faaac468d4f5478c2a23f78c75e66bb85189b2
CmEinsig <cmneiderer@gmail.com>
2023-03-24 16:24:53 -0400

    worked on password length parameters and alerts

--- Assets/script.js
+++ Assets/script.js
@@ -12,54 +12,35 @@ lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
 "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 numbers = [1,2,3,4,5,6,7,8,9]
-special = [" !@#$%^&*()"]
+special = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')','(','}','{',']', '[', '~', '-', '_', '.',];
+
 
 // Write password to the #password input
 var generatepassword =function () {
 var length = window.prompt("Enter a password length between 8 and 128 characters")
-var lowercase = window.prompt("Do you want lowercase letters? type yes or no.")
-var uppercase = window.prompt("Do you want uppercase letters? type yes or no.")
-var numbers = window.prompt("Do you want to use numbers? type yes or no.")
-var special = window.prompt("Do you want to use special characters? type yes or no.")
+var lowercase = window.confirm("Do you want lowercase letters? If no, click cancel")
+var uppercase = window.confirm("Do you want uppercase letters? If no, click cancel")
+var numbers = window.confirm("Do you want to use numbers? If no, click cancel")
+var special = window.confirm("Do you want to use special characters? If no, click cancel")
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
-//convert choices for compare
-lowercase = lowercase.toUpperCase();
-uppercase = uppercase.toUpperCase();
-numbers = numbers.toUpperCase();
-special = special.toUpperCase();
+  // Password Length
+  var length = parseInt(prompt("Enter a password length between 8 and 128 characters"));
+  var finalChar = [];
+  var possibleChar = [];
+  var options = [];
+if(Number.isNaN(length)){
+    alert("Password length must be a number");
+    generatePassword();
+  }
+if(length > 8 || length > 128)
+  {
+    alert("Password length must be between 8 and 128 characters");
+    generatePassword();
+  }
 
-if (lowercase === yes){
-}
-else if (lowercase === no) {
-}
-else {  
-window.prompt ("Please enter yes or no")
-}
 
-if (uppercase === yes){
-}
-else if (uppercase === no) {
-}
-else {  
-window.prompt ("Please enter yes or no")
-}
-
-if (numbers === yes){
-}
-else if (numbers === no) {
-}
-else {  
-window.prompt ("Please enter yes or no")
-}
-
-if (special === yes){
-
-}
-else if (special === no) {
-}
-else {  
-window.prompt ("Please enter yes or no")
-}
 
 
 
@@ -68,6 +49,6 @@ passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
 
 generatepassword ()
