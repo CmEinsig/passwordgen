3d840b0f32a393b8c1fb44bf88c0c368810f669e
CmEinsig <cmneiderer@gmail.com>
2023-03-24 20:08:17 -0400

    added README

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var passwordlength = document.getElementById("length");
-var includelowercase = document.getElementById("lowercase")
-var includeuppercase = document.getElementById("uppercase")
-var includenumbers = document.getElementById("numbers")
-var specialCharacters =document.getElementById("special")
 //Arrays for computer to choose from 
 length = [8 <= 128]
 lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
@@ -16,7 +9,7 @@ special = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')'
 
 
 // Write password to the #password input
-var generatepassword =function () {
+var generatepassword = function () {
 var length = window.prompt("Enter a password length between 8 and 128 characters")
 var lowercase = window.confirm("Do you want lowercase letters? If no, click cancel")
 var uppercase = window.confirm("Do you want uppercase letters? If no, click cancel")
@@ -28,15 +21,7 @@ var special = window.confirm("Do you want to use special characters? If no, clic
   var finalChar = [];
   var possibleChar = [];
   var options = [];
-if(Number.isNaN(length)){
-    alert("Password length must be a number");
-    generatePassword();
-  }
-if(length < 8 || length > 128)
-  {
-    alert("Password length must be between 8 and 128 characters");
-    generatePassword();
-  }
+
 
 if(uppercase === false && lowercase === false && special === false && numbers === false)
   {
@@ -86,13 +71,14 @@ if(special)
     return possibleChar.join("");
   }
 
-
+  var generateBtn = document.querySelector("#generate");
+function writePassword() {
   var password = generatePassword();
 var passwordText = document.querySelector("#password");
 passwordText.value = password;
 
 
-
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # passwordgen
 Javascript Password generator 
+
+## Description
+My motivation for this project was to test my new skills in JavaScript. I built this project to create random passwords. This solved the problem of having to come up with a new password all of the time' I can input some information and I can get a random password. I learned that JavaScript is not an easy language. There are a lot of variables and positions that you have to keep track of.  
+
+## Installation
+None 
+
+## Usage
+
+Usage is available on the website. You can click the password prompts to get a random password. 
+
+
+![Screenshot of portfolio webpage](./Assets/screencapture-file-C-Users-Courtney-challenges-passwordgen-index-html-2023-03-24-20_03_51.png)
+
+## Credits
+
+None 
+
+## License
+
+None
